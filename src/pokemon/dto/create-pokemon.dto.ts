7101b593be572a4b616b6796f315452d43618db6
Tiago <tiago.appdev@gmail..com>
2024-03-06 13:46:07 -0300

    first commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+  @IsInt()
+  @Min(1)
+  @IsPositive()
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
